7a62eaeaf70428d375431746bba2fcef44c19fc9
desydv <desychristy23@gmail.com>
2018-07-25 14:02:33 +0700

    add controller to project

---
+++ app/controllers/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+final class ApiController{
+    public function hello(Request $request, Response $response, $args)
+    {
+        $dataRes =[
+            'apiVersion' => getenv('APP_VERSION'),
+            'data' => [
+                'serverTime' => date("Y-m-d H:i:s"), 
+            ]
+        ];
+        return $response->withJSON($dataRes,200,JSON_UNESCAPED_UNICODE);
+    }
+    
+}
--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-use Nette\Mail\Message;
 use App\Models\Customer;
+use App\Controllers\CustomerController;
+use App\Controllers\ApiController;
 require '../vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
@@ -12,60 +13,16 @@ $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 require __DIR__ . '/../src/dependencies.php';
 
-$app->get('/', function ($request, $response, $args) {
-    $this->logger->info("/");
-    $dataRes =[
-            'apiVersion' => getenv('APP_VERSION'),
-            'data' => [
-                'serverTime' => date("Y-m-d H:i:s"), 
-            ]
-        ];
-   return $response->withJSON($dataRes,200,JSON_UNESCAPED_UNICODE);
-});
+$app->get('/', ApiController::class.':hello');
 
-$app->get('/customers', function ($request, $response, $args) {
-    $customers = Customer::all();
-    return $this->response->withJson(["status" => "success", "data" => $customers], 200);
-});
+$app->get('/customers', CustomerController::class.':getCustomers');
 
-$app->get('/customer/[{id}]', function ($request, $response, $args) {
-    $customer = Customer::find($args['id']);
-    return $this->response->withJson(["status" => "success", "data" => $customer], 200);
-});
+$app->get('/customer/[{id}]', CustomerController::class.':getSpecificCustomer');
 
-$app->post('/customer', function ($request, $response) {
-    $input = $request->getParsedBody();
-    $customer = Customer::create($input);
-    if($customer){
-        return $response->withJson(["status" => "success", "data" => $customer], 200);
-    }else{
-        return $response->withJson(["status" => "failed", "data" => $customer], 200);
-    }
-});
+$app->post('/customer', CustomerController::class.':createCustomer');
 
-$app->delete('/customer/[{id}]', function ($request, $response, $args) {     
-    $customer = Customer::destroy($args['id']);
-    if($customer){
-        return $response->withJson(["status" => "success", "data" => $customer], 200);
-    }else{
-        return $response->withJson(["status" => "failed", "data" => $customer], 200);
-    }
-});
+$app->put('/customer/[{id}]', CustomerController::class.':updateCustomer');
 
-$app->put('/customer/[{id}]', function ($request, $response, $args) {
-    $input = $request->getParsedBody();
+$app->delete('/customer/[{id}]', CustomerController::class.':deleteCustomer');
 
-    $customer = Customer::find($args['id']);
-    $customer->email = $input['email'];
-    $customer->name = $input['name'];
-    $customer->password = $input['password'];
-    $customer->phone = $input['phone'];
-    $customer->save();
-   
-    if($customer){
-        return $response->withJson(["status" => "success", "data" => $customer], 200);
-    }else{
-        return $response->withJson(["status" => "failed", "data" => $customer], 200);
-    }
-});
 $app->run();
